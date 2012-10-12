2c93d90d86fa84914560cb06cda2ca827a9db7ed
Reedy <reedy@wikimedia.org>
2012-10-12 15:48:14 +0100

    Initial commit
    Change-Id: Id5c36e4d21a18a7a6d13e3bdda8c7950b36abf3d

---
+++ RelatedArticles.i18n.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Internationalisation file for RelatedArticles extension.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+    'ra-RelatedArticles' => 'Related articles',
+    'ra-desc'            => 'Extension for presentation of related articles on the sidebar',
+);
+
+$messages['de'] = array(
+    'ra-RelatedArticles' => 'Verwandte Artikel',
+    'ra-desc'            => 'Erweiterung für die Ausgabe von verwandten Artikeln auf der Sidebar',
+);
+
+$messages['fr'] = array(
+    'ra-RelatedArticles' => 'Articoli collegati',
+);
+
+$messages['it'] = array(
+    'ra-RelatedArticles' => 'Autres articles',
+);
+
+?>
