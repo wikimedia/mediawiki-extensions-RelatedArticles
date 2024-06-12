dd5a18c1fc3615424579909408aca292d62e333f
Umherirrender <umherirrender_de.wp@web.de>
2024-06-12 20:38:55 +0200

    tests: Use renamed class BundleSizeTestBase
    Follow-Up: I131e0d85bd14e75aaebd6f212b2e64a45d4c73a2
    Change-Id: I071827d4f046ffc7ad545094b146912db7c43fc5

--- tests/phpunit/structure/BundleSizeTest.php
+++ tests/phpunit/structure/BundleSizeTest.php
@@ -2,7 +2,7 @@
 
 namespace MediaWiki\Extensions\Minerva\Tests\Structure;
 
-class BundleSizeTest extends \MediaWiki\Tests\Structure\BundleSizeTest {
+class BundleSizeTest extends \MediaWiki\Tests\Structure\BundleSizeTestBase {
 
 	/** @inheritDoc */
 	public function getBundleSizeConfig(): string {
