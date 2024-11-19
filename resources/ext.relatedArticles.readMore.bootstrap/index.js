0e6084246c589d491508fed92c0aa40252b8e0c1
Jon Robson <jdlrobson@gmail.com>
2024-11-19 08:33:26 -0800

    Wire RelatedArticles up to empty search hook and experiment configuration
    Group experimentEnabled: Shows experiment
    Group experimentDisabled: No change
    Others: No change
    
    Note any group containing the sdtring -sampled will not trigger
    an instrumentation event, so that we can control traffic.
    
    Styling will be done in a follow up
    
    Bug: T378115
    Bug: T378101
    Change-Id: I2526f12e6843cc58e1ea9a92cfcde1657d3c8d2e

--- resources/ext.relatedArticles.readMore.bootstrap/index.js
+++ resources/ext.relatedArticles.readMore.bootstrap/index.js
@@ -1,56 +1,91 @@
-( function () {
+const config = require( './config.json' );
+
+/**
+ * Load related articles when the user scrolls past half of the window height.
+ *
+ * @ignore
+ */
+function loadRelatedArticles() {
+	const readMore = document.querySelector( '.read-more-container' );
+
 	/**
-	 * Load related articles when the user scrolls past half of the window height.
-	 *
-	 * @ignore
+	 * @param {Element} container
 	 */
-	function loadRelatedArticles() {
-		const readMore = document.querySelector( '.read-more-container' );
-
-		if ( !readMore ) {
-			// The container is not in the HTML for some reason and cannot be queried.
-			// See T281547
-			return;
-		}
-
-		/**
-		 * @param {Element} container
-		 */
-		function initRelatedArticlesModule( container ) {
-			$.when(
-				mw.loader.using( 'ext.relatedArticles.readMore' )
-			).then( (
-				/** @type {Function} */ require
-			) => {
-				require( 'ext.relatedArticles.readMore' ).init(
-					container
-				);
-			} );
-		}
-
-		const doc = document.documentElement;
-		// IntersectionObserver will not work if the component is already visible on the page.
-		// To handle this case, we compare scroll height to viewport height.
-		if ( ( doc.scrollHeight / 2 ) < doc.clientHeight ) {
-			// Load straight away. We are on a stub page.
-			initRelatedArticlesModule( readMore );
-			return;
-		}
-		// eslint-disable-next-line compat/compat
-		const observer = /** @type {IntersectionObserver} */( new IntersectionObserver( ( ( entries ) => {
-			if ( !entries[ 0 ].isIntersecting ) {
-				return;
-			}
-			// @ts-ignore
-			observer.unobserve( readMore );
-			observer.disconnect();
-			// @ts-ignore
-			initRelatedArticlesModule( readMore );
-		} ), {
-			rootMargin: '-100% 0% 0% 0%'
-		} ) );
-		observer.observe( readMore );
+	function initRelatedArticlesModule( container ) {
+		$.when(
+			mw.loader.using( 'ext.relatedArticles.readMore' )
+		).then( (
+			/** @type {Function} */ require
+		) => {
+			require( 'ext.relatedArticles.readMore' ).init(
+				container
+			);
+		} );
 	}
 
-	$( loadRelatedArticles );
-}() );
+	/**
+	 * @param {Element} node
+	 */
+	const emptySearchHook = ( node ) => {
+		let relatedContainer = node.querySelector( '.ext-related-empty-search' );
+		if ( !relatedContainer ) {
+			relatedContainer = document.createElement( 'div' );
+			relatedContainer.setAttribute( 'class', 'ext-related-empty-search' );
+			node.appendChild( relatedContainer );
+		}
+		relatedContainer.removeAttribute( 'style' );
+		initRelatedArticlesModule( relatedContainer );
+	};
+
+	/**
+	 * Enables recommendations in search results.
+	 */
+	function loadEmptySearchHook() {
+		mw.hook( 'ext.MobileFrontend.searchOverlay.empty' ).add( emptySearchHook );
+	}
+
+	const experiment = config.RelatedArticlesABTestEnrollment;
+	const group = mw.experiments.getBucket(
+		experiment,
+		mw.user.sessionId()
+	);
+	const WEB_AB_TEST_ENROLLMENT_HOOK = 'mediawiki.web_AB_test_enrollment';
+	if ( experiment.enabled && group.indexOf( '-unsampled' ) === -1 ) {
+		mw.hook( WEB_AB_TEST_ENROLLMENT_HOOK ).fire( {
+			group,
+			experimentName: experiment.name
+		} );
+		if ( group === 'experimentEnabled' ) {
+			loadEmptySearchHook();
+		}
+	}
+	if ( !readMore ) {
+		// The container is not in the HTML for some reason and cannot be queried.
+		// See T281547
+		return;
+	}
+	const doc = document.documentElement;
+	// IntersectionObserver will not work if the component is already visible on the page.
+	// To handle this case, we compare scroll height to viewport height.
+	if ( ( doc.scrollHeight / 2 ) < doc.clientHeight ) {
+		// Load straight away. We are on a stub page.
+		initRelatedArticlesModule( readMore );
+		return;
+	}
+	// eslint-disable-next-line compat/compat
+	const observer = /** @type {IntersectionObserver} */( new IntersectionObserver( ( ( entries ) => {
+		if ( !entries[ 0 ].isIntersecting ) {
+			return;
+		}
+		// @ts-ignore
+		observer.unobserve( readMore );
+		observer.disconnect();
+		// @ts-ignore
+		initRelatedArticlesModule( readMore );
+	} ), {
+		rootMargin: '-100% 0% 0% 0%'
+	} ) );
+	observer.observe( readMore );
+}
+
+$( loadRelatedArticles );
