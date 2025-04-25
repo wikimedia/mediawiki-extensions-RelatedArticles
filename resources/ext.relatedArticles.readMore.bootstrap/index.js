e8581b23ef7b220cc7a5e7b703ec86b930ea3c2e
bernardwang <bwang@wikimedia.org>
2025-04-25 15:34:46 -0500

    Remove Search AB test code
    Bug: T388719
    Change-Id: Ic771375444b48898ed812b04fdffd0bfdeb87802

--- resources/ext.relatedArticles.readMore.bootstrap/index.js
+++ resources/ext.relatedArticles.readMore.bootstrap/index.js
@@ -1,32 +1,4 @@
-const config = require( './config.json' );
 const CLICK_EVENT_FOOTER = 'relatedArticles.footer';
-const CLICK_EVENT_EMPTY_SEARCH = 'relatedArticles.emptySearch';
-const STORED_AB_BUCKET_KEY = 'relatedArticles.token';
-
-/**
- * Get AB test token, factoring in locally stored bucket and URL override.
- *
- * @return String - Token used for AB test bucketing
- */
-function getABTestToken() {
-	// @ts-ignore FIXME mw.storage.set/get should be added to types-mediawiki repo.
-	const storedToken = mw.storage.get( STORED_AB_BUCKET_KEY );
-	const storageExpiry = 90 * 24 * 60 * 60; // 90 days in seconds.
-	let bucketingToken;
-
-	// If a valid AB test token exists in localStorage, use that.
-	if ( storedToken ) {
-		bucketingToken = storedToken;
-	} else {
-		bucketingToken = mw.user.sessionId();
-	}
-
-	// Store the bucketing token for 90 days.
-	// @ts-ignore FIXME mw.storage.set/get should be added to types-mediawiki repo.
-	mw.storage.set( STORED_AB_BUCKET_KEY, bucketingToken, storageExpiry );
-
-	return bucketingToken;
-}
 
 /**
  * Load related articles when the user scrolls past half of the window height.
@@ -53,41 +25,6 @@ function loadRelatedArticles() {
 		} );
 	}
 
-	/**
-	 * @param {Element} node
-	 */
-	const emptySearchHook = ( node ) => {
-		let relatedContainer = node.querySelector( '.ext-related-empty-search' );
-		if ( !relatedContainer ) {
-			relatedContainer = document.createElement( 'div' );
-			relatedContainer.setAttribute( 'class', 'ext-related-empty-search' );
-			node.appendChild( relatedContainer );
-		}
-		relatedContainer.removeAttribute( 'style' );
-		initRelatedArticlesModule( relatedContainer, CLICK_EVENT_EMPTY_SEARCH );
-	};
-
-	/**
-	 * Enables recommendations in search results.
-	 */
-	function loadEmptySearchHook() {
-		mw.hook( 'ext.MobileFrontend.searchOverlay.empty' ).add( emptySearchHook );
-	}
-
-	const experiment = config.RelatedArticlesABTestEnrollment;
-	const bucketingToken = getABTestToken();
-	const group = mw.experiments.getBucket( experiment, bucketingToken );
-
-	const WEB_AB_TEST_ENROLLMENT_HOOK = 'mediawiki.web_AB_test_enrollment';
-	if ( experiment.enabled && group.indexOf( '-unsampled' ) === -1 ) {
-		mw.hook( WEB_AB_TEST_ENROLLMENT_HOOK ).fire( {
-			group,
-			experimentName: experiment.name
-		} );
-		if ( group === 'experimentEnabled' ) {
-			loadEmptySearchHook();
-		}
-	}
 	if ( !readMore ) {
 		// The container is not in the HTML for some reason and cannot be queried.
 		// See T281547
@@ -118,9 +55,3 @@ function loadRelatedArticles() {
 }
 
 $( loadRelatedArticles );
-
-// Exports for unit testing.
-module.exports = {
-	getABTestToken: getABTestToken,
-	storageKey: STORED_AB_BUCKET_KEY
-};
